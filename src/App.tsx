7f139bf116951204dce6cefdad65fe2e0dd1d69a
fabiansilvera <fabiansilvera@gmail.com>
2025-07-04 17:43:56 -0300

    Cambios en Proyecto creacion de Types, hook de ordenesy component del menu de items

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import MenuItem from "./components/MenuItem"
 import { menuItems } from "./data/db"
+import useOrder from "./hooks/useOrder"
 
 function App() {
+
+  const { addItem } = useOrder()
 
   return (
     <>
@@ -11,11 +14,18 @@ function App() {
 
       <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
 
-        <div>
-          <h2>Menú</h2>
-          {menuItems.map(item => (
-            <MenuItem/>
-          ))}
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menú</h2>
+
+          <div className="space-y-3 mt-10">
+            {menuItems.map(item => (
+              <MenuItem
+                key={item.id}
+                item={item}
+                addItem={addItem}
+              />
+            ))}
+          </div>
         </div>
         
         <div>
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,5 +1,15 @@
-export default function MenuItem() {
+import type { MenuItem } from "../types"
+
+type MenuItemProps = {
+  item: MenuItem
+  addItem: () => void
+}
+
+export default function MenuItem({item, addItem} : MenuItemProps) {
   return (
-    <div>MenuItem</div>
+    <button className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between " onClick={() => addItem()}>
+      <p>{item.name}</p>
+      <p className="font-black">${item.price}</p>
+    </button>
   )
 }
