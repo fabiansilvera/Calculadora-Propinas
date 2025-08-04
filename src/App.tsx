5e76d07479056807cfad81340cc902a410e5f278
fabiansilvera <fabiansilvera@gmail.com>
2025-08-04 13:50:37 -0300

    Fin del front-end del proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import MenuItem from "./components/MenuItem"
+import OrderContents from "./components/OrderContents"
+import OrderTotals from "./components/OrderTotals"
+import TipPorcentageForm from "./components/TipPorcentageForm"
 import { menuItems } from "./data/db"
 import useOrder from "./hooks/useOrder"
 
 function App() {
 
-  const { addItem } = useOrder()
+  const { order,tip, setTip, addItem, removeItem, placeOrder } = useOrder()
 
   return (
     <>
@@ -28,8 +31,27 @@ function App() {
           </div>
         </div>
         
-        <div>
-          <h2>Consumo</h2>
+        <div className="border border-dashed border-slate-400 p-5 rouded-lg space-y-10">
+          {order.length ? (
+            <>
+              <OrderContents
+                order={order}
+                removeItem={removeItem}
+              />
+
+              <TipPorcentageForm
+                setTip={setTip}
+                tip={tip}
+              />
+
+              <OrderTotals
+                order={order}
+                tip={tip}
+                placeOrder={placeOrder}
+              />
+            </>
+          ) : <p className="text-center">La orden esta vacia</p>}
+           
         </div>
         
       </main>
---
+++ src/components/OrderContents.tsx
@@ -0,0 +1,32 @@
+import { formatCurrency } from "../helpers"
+import type { MenuItem, OrderItem } from "../types"
+
+type OrderContentsProps = {
+    order: OrderItem[],
+    removeItem: (id : MenuItem['id']) => void
+}
+
+export default function OrderContents({order, removeItem} : OrderContentsProps) {
+  return (
+    <div>
+        <h2 className="font-black text-4xl">Consumo</h2>
+
+        <div className="space-y-3 mt-10">
+            {order.map( item => (
+                <div key={item.id} className="flex justify-between border-t items-center border-gray-300 py-5 last-of-type:border-b">
+                    <div>
+                        <p className="text-lg">{item.name} - {formatCurrency(item.price)}</p>
+                        <p className="font-black">
+                            Cantidad: {item.quantity} - { formatCurrency(item.price * item.quantity) }
+                        </p>
+                    </div>
+
+                    <button className="bg-red-600 h-8 w-8 rounded-full text-white font-black cursor-pointer" onClick={() => removeItem(item.id)}>
+                        X
+                    </button>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
